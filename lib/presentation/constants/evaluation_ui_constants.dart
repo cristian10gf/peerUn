7234030e2dcf23fa2f398e81d6028bb8f49f01cb
cristian gonzalez franco <97616425+cristian10gf@users.noreply.github.com>
2026-03-23 16:10:35 -0500

    feat: Add EvaluationUiConstants class with criteria labels and colors

---
+++ lib/presentation/constants/evaluation_ui_constants.dart
@@ -0,0 +1,15 @@
+class EvaluationUiConstants {
+  static const List<String> criteriaLabels = [
+    'PUNTU',
+    'CONTRIB',
+    'COMPRO',
+    'ACTITU',
+  ];
+
+  static const List<double> criteriaColors = [
+    0xFF60A5FA,
+    0xFFA78BFA,
+    0xFF34D399,
+    0xFFF9A8D4,
+  ];
+}
